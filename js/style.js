20ba3a16d05b9e207c59680439befb347fd7cb9b
ZJing <izengjing.cn@gmail.com>
2017-04-19 16:35:24 +0800

    正式引用sass 完成对header的改造

---
+++ js/style.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ZJing on 2017/4/19.
+ */
+
+
+require(['jquery'],function(){
+    var $table = $("#table");
+    var $showLi = $("#nav li");
+    $showLi.hover(function(){
+        console.log(111);
+        $table.show();
+    },function(){
+        console.log(222);
+        $table.hide();
+    });
+});
