d9a83ddcd5d56071d2addbed19b10f65f89851fe
ZJing <izengjing.cn@gmail.com>
2017-04-22 20:16:10 +0800

    购物车---解决一些问题 （解决昨天jquery传值的问题 问题原因： jquery版本的原因； 描述：错误很低级，但是这种错误很磨人，翻遍整个代码找bug，耗费了大量精力，找了的大神帮忙，最后才发现是jquery版本的问题，这就好比，你走遍全世界抓小偷，却没有想到，小偷早就卧底你的身边，值得注意； ）

--- js/index.js
+++ js/index.js
@@ -7,20 +7,43 @@
 
 $(function(){
     //商品功能
-    var Product = function (id,name,price,img){
+    var Product = function (id,name,price,img,quantity){
         this.id = id;
         this.name = name;
         this.price = price;
         this.img = img;
-        // this.quantity = quantity;
+        this.quantity = 0;
     };
     var cart = {
         totalQuantity: 0,
         totalAmount: 0,
-        addCart: function(){
+        productList:[],
+        addCart: function(product){
+            // 版本一
+            // this.productList.push(product);
+            // this.totalQuantity = 0;this.totalAmount = 0;
+            // for(var i = 0;i<this.productList.length;i++){
+            //     this.totalQuantity += this.productList[i].quantity;
+            //     this.totalAmount += this.productList[i].price * this.productList[i].quantity;
+            // }
+            // 版本二
+            //对上面的代码改进：每次点击，都要遍历整个productList，麻烦，但实际只需要改变当前长度里面的属性值就可以了
+            //this.productList.push(product);
+            // this.totalQuantity += this.productList[this.productList.length].quantity;
+            // this.totalAmount += this.productList[this.productList.length].price * this.productList[this.productList.length].quantity;
+            // 版本三
+            //再改进（优化版）
+            this.productList.push(product);
+            this.totalQuantity += product.quantity;
+            this.totalAmount += product.price * product.quantity;
 
+            //优化：购物车方法，这个是业务逻辑，不应该dom操作，不应该与dom直接挂钩，应该把dom操作与业务逻辑分离开
+            $('#quantity').html(this.totalQuantity+" items");
+            $('#money').html("$ "+this.totalAmount);
+            // console.log(this.productList.length);
+            // console.log(this.productList);
         },
-        remoueCart: function(){
+        removeCart: function(){
 
         }
     };
@@ -29,19 +52,24 @@ $(function(){
         init : function(){
             this.loadData();//先加载一部分数据
             this.$productList.on('click','.btn-add-cart',function(){
-
-                console.log($(this).parents('.product-item'));
-                console.log($(this).parents('.product-item').data());
-                console.log($(this).parents('.product-item').data('item-data'));
+                var product = $(this).parents('.product-item').data('item-data');
+                product.quantity = parseInt($(this).prev().val());//获取数量
+                // product.price = product.quantity*product.price;
+                // cart.addCart($(this).prev().val()*)
+                cart.addCart(product);
+               //   console.log($(this).parents('.product-item'));
+                // console.log($(this).parents('.product-item'));
+                //  console.log($(this).parents('.product-item').data('item-data'));
+                // console.log(product);
             });
-
         },
         loadData : function(){
             // var $productList = $('#bestSeller ul');
             $.get('js/data.json',{},function (data) {//【【【获取数据】】】url,一个数据，一个回掉函数，返回一个数据格式'json'(一堆商品)
                 for(var i = 0;i<data.length;i++){
 
-                    var product = new Product(data[i].product_id,data[i].product_name,data[i].product_price,data[i].product_img);
+                    var product = new Product(data[i].product_id,data[i].product_name,data[i].product_price,
+                                            data[i].product_img,data[i].product_quantity);
                     var $product = $('<li class="product-item">\
                             <div class="imgZone">\
                                 <a href=""><img src="'+product.img+'" alt=""></a>\
@@ -49,15 +77,12 @@ $(function(){
                             <div class="product-info">\
                                 <h3 class="product-name">'+product.name+'</h3>\
                                 <h4 class="product-price">'+product.price+'</h4>\
-                                <input type="text" class="quantity" value="1"/><br />\
+                                <input type="text" class="quantity" value="'+product.quantity+'"/>\
                                 <input type="button" class="btn-add-cart" value="ADD">\
                             </div>\
-                        </li>');//绑定 名称：item-data，值：product
-                    $product.data('item-data',product);
+                        </li>');
+                    $product.data('item-data',product);//绑定 名称：item-data，值：product
                     this.$productList.append($product);
-
-                    console.log($product);
-                     console.log($product.data('item-data'));
                 }
             }.bind(this),'json');//是个函数，通过bind把this传进去
         },
